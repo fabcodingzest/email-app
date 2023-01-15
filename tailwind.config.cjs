c171df4110744d2c18b6d1cf8a3a1b7d367052bd
fabcodingzest <fabrizvi786@gmail.com>
2023-01-16 00:08:36 +0530

    added: theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E54065',
+        secondary: '#636363',
+        neutral: '#CFD2DC',
+        light: '#F2F2F2',
+        btn: '#E1E4EA',
+      },
+    },
   },
   plugins: [],
 }
