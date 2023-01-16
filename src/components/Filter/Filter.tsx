e76b6bb379778d817ae31f3ae392e2a067f00988
fabcodingzest <fabrizvi786@gmail.com>
2023-01-17 00:55:30 +0530

    added: state and actions to Filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Text from '../common/Text'
 import Chip from '../common/Chip'
 
 const Filter = () => {
-  const [activeFilter, setActiveFilter] = useState('Unread')
   return (
     <div className='flex items-center gap-4 pb-4'>
       <Text fontSize='text-xs' color='text-black' text='Filter By:' />
-      <Chip text='Unread' filter={{ setActiveFilter, activeFilter }} />
-      <Chip text='Read' filter={{ setActiveFilter, activeFilter }} />
-      <Chip text='Favourite' filter={{ setActiveFilter, activeFilter }} />
+      <Chip text='All' />
+      <Chip text='Unread' />
+      <Chip text='Read' />
+      <Chip text='Favourite' />
     </div>
   )
 }
--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import { useAppSelector } from '../../App/hooks'
+import { useDispatch } from 'react-redux'
+import { setActiveFilter } from '../../features/email/emailSlice'
 
 interface ChipProps {
   text: string
-  filter: {
-    setActiveFilter: React.Dispatch<React.SetStateAction<string>>
-    activeFilter: string
-  }
 }
 
-const Chip = ({ text, filter: { activeFilter, setActiveFilter } }: ChipProps) => {
+const Chip = ({ text }: ChipProps) => {
+  const filter = useAppSelector((state) => state.email.activeFilter)
+  const dispatch = useDispatch()
+
   return (
     <button
       className={`rounded-full border px-2 py-[0.1rem] text-xs  ${
-        activeFilter === text
+        filter === text.toLowerCase()
           ? 'bg-btn border-neutral text-secondary'
           : 'border-transparent text-black'
       }`}
-      onClick={() => setActiveFilter(text)}
+      onClick={() => dispatch(setActiveFilter(text.toLowerCase()))}
     >
       {text}
     </button>
