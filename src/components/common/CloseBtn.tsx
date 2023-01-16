66d6762263f3aa02f66746d9f8a5d861b8da17a3
fabcodingzest <fabrizvi786@gmail.com>
2023-01-17 05:26:49 +0530

    minor color change

--- src/components/common/CloseBtn.tsx
+++ src/components/common/CloseBtn.tsx
@@ -5,7 +5,7 @@ const CloseBtn = () => {
   const dispatch = useDispatch()
   return (
     <button
-      className='absolute -right-3 -top-3 flex h-6 w-6 items-center justify-center rounded-full border-2 border-gray-500 bg-gray-300 p-[0.2rem] text-sm font-extrabold'
+      className='text-secondary absolute -right-3 -top-3 flex h-6 w-6 items-center justify-center rounded-full border-2 border-gray-400 bg-gray-300 p-[0.2rem] text-sm font-extrabold'
       onClick={() => dispatch(removeActiveEmail())}
     >
       x
