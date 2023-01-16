499706f831af17bdd24042a9ef7f94971cafee2d
fabcodingzest <fabrizvi786@gmail.com>
2023-01-17 00:52:02 +0530

    added: state from store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
 import Container from './components/Layout/Container'
 import EmailList from './components/Email/EmailList'
 import Filter from './components/Filter/Filter'
 import EmailDetails from './components/Email/EmailDetails'
+import { useAppSelector } from './App/hooks'
+import { useFetchEmailsQuery } from './App/services/api'
 
 const App = () => {
-  const [detailActive, setDetailActive] = useState('')
-  const detailOpen = detailActive !== ''
+  const activeEmail = useAppSelector((state) => state.email.activeEmail)
+  const detailOpen = activeEmail !== ''
+  const { isLoading } = useFetchEmailsQuery(1)
+
   return (
-    <div className='text-secondary bg-[#F4F5F9] py-2 md:py-4'>
+    <div className='text-secondary min-h-screen bg-[#F4F5F9] py-2 md:py-4'>
       <Container>
         <Filter />
         <div className='flex gap-4'>
-          <EmailList
-            details={{ detailActive, setDetailActive }}
-            width={detailOpen ? 'w-2/6' : 'w-full'}
-          />
+          {isLoading ? (
+            <p>LOADING JAMASHIMA......</p>
+          ) : (
+            <EmailList width={detailOpen ? 'w-2/6' : 'w-full'} />
+          )}
           {detailOpen && <EmailDetails />}
         </div>
       </Container>
