9913e6d0960f09a9cbb686a0b7c83edc326a84a4
fabcodingzest <fabrizvi786@gmail.com>
2023-01-17 21:57:05 +0530

    added: Pagination for query and filters both

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,26 @@ import { useAppSelector } from './App/hooks'
 import { useFetchEmailsQuery } from './App/services/api'
 import Error from './components/common/Error'
 import Loader from './components/common/Loader'
+import { useState } from 'react'
+import Pagination from './components/common/Pagination'
+import { getFilteredEmails } from './utils/helper'
+
+export const EMAIL_PER_PAGE = 10
 
 const App = () => {
-  const activeEmail = useAppSelector((state) => state.email.activeEmail)
-  const detailOpen = activeEmail !== ''
-  const { isLoading, error } = useFetchEmailsQuery(1)
+  const [queryCurrPage, setQueryCurrPage] = useState(1)
+  const [arrayCurrPage, setArrCurrPage] = useState(1)
+  const { data, isLoading, isFetching, isSuccess, error } = useFetchEmailsQuery(queryCurrPage)
+  const state = useAppSelector((state) => state.email)
+  const filter = state.activeFilter
+  const currentEmails = getFilteredEmails(state, filter)
+  const detailOpen = state.activeEmail !== ''
+  const isAllFilter = filter === 'all'
+  const totalPages = Math.ceil(
+    (isSuccess && isAllFilter ? data?.total : currentEmails.length) / EMAIL_PER_PAGE,
+  )
+  const currPage = isAllFilter ? queryCurrPage : arrayCurrPage
+  const setCurrPage = isAllFilter ? setQueryCurrPage : setArrCurrPage
 
   return (
     <div className='text-secondary min-h-screen bg-[#F4F5F9] py-2 md:py-4'>
@@ -19,9 +34,22 @@ const App = () => {
           <Error error={error} />
         ) : (
           <>
-            <Filter />
+            <div className='flex items-center justify-between'>
+              <Filter />
+              {!detailOpen && totalPages > 0 && (
+                <Pagination currPage={currPage} totalPages={totalPages} setPage={setCurrPage} />
+              )}
+            </div>
             <div className='flex gap-4'>
-              {isLoading ? <Loader /> : <EmailList width={detailOpen ? 'w-2/6' : 'w-full'} />}
+              {isLoading || isFetching ? (
+                <Loader />
+              ) : (
+                <EmailList
+                  currPage={currPage}
+                  list={currentEmails}
+                  width={detailOpen ? 'w-2/6' : 'w-full'}
+                />
+              )}
               {detailOpen && <EmailDetails />}
             </div>
           </>
