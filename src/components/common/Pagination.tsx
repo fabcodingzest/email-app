fef94f10a285dc40cbb2baa819c42a752da31b98
fabcodingzest <fabrizvi786@gmail.com>
2023-01-18 03:25:05 +0530

    added: disabled prop and styles to btns

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -8,18 +8,22 @@ interface PaginationProps {
 interface PageBtnProps {
   direction: string
   onClick: () => void
+  disabled: boolean
 }
-const PageBtn = ({ onClick, direction }: PageBtnProps) => {
-  const next = direction === 'next'
-
+const PageBtn = ({ onClick, direction, disabled }: PageBtnProps) => {
+  const isNext = direction === 'next'
+  console.log(disabled)
   return (
     <button
-      className={'flex h-5 w-5 items-center justify-center rounded-full '}
+      className={
+        'group flex h-1 w-1 items-center justify-center rounded-full active:translate-y-[0.05rem] disabled:opacity-25 md:h-4 md:w-4'
+      }
+      disabled={disabled}
       onClick={() => onClick()}
     >
       <span
-        className={`h-0 w-0 border-x-8 border-b-[16px] border-x-transparent border-b-black ${
-          next ? 'rotate-90' : '-rotate-90'
+        className={`h-0 w-0 border-x-8 border-b-[0.5rem] border-x-transparent border-b-black group-hover:border-b-gray-600 ${
+          isNext ? 'rotate-90' : '-rotate-90'
         }`}
       ></span>
     </button>
@@ -27,17 +31,19 @@ const PageBtn = ({ onClick, direction }: PageBtnProps) => {
 }
 
 const Pagination = ({ currPage, totalPages, setPage }: PaginationProps) => {
+  const haveNext = currPage !== totalPages
+  const havePrev = currPage !== 1
   const nextPage = () => {
-    if (currPage !== totalPages) setPage(currPage + 1)
+    if (haveNext) setPage(currPage + 1)
   }
   const prevPage = () => {
-    if (currPage !== 1) setPage(currPage - 1)
+    if (havePrev) setPage(currPage - 1)
   }
   return (
-    <div className='flex items-center gap-4 pb-2'>
-      <PageBtn direction='prev' onClick={prevPage} />
+    <div className='flex items-center gap-2 text-xs md:text-sm '>
+      <PageBtn direction='prev' onClick={prevPage} disabled={!havePrev} />
       {currPage} of {totalPages}
-      <PageBtn direction='next' onClick={nextPage} />
+      <PageBtn direction='next' onClick={nextPage} disabled={!haveNext} />
     </div>
   )
 }
